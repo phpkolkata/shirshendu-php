a203fdae7b192d8777b12cd4f42484143c1f8063
Md Asif Iqbal <phpkolkata@gmail.com>
2022-02-21 17:21:09 +0530

    php start

---
+++ php/1.php
@@ -0,0 +1,13 @@
+<b>PHP PAGE</b>
+
+<!-- html area -->
+<?php
+// php area
+// <b>bold</b>
+print "<b>bold</b>";
+
+
+?>
+<!-- html area -->
+
+
---
+++ php/2.php
@@ -0,0 +1,24 @@
+<?php
+
+// varibale(container -temporary memory location) & data type (type of content)
+
+// string - "abc"
+// integer - 123
+// float - 22.22
+// boolean - yes / no, 0/1, true/false
+
+// auto type casting
+// $x = 10; // integer
+// $x = "abc"; // string 
+
+
+$x = 10;
+$y = 20;
+$z = $x + $y;
+
+print $z;
+
+// home work - 
+// run php 
+// read variables and data type
+// read php operators
